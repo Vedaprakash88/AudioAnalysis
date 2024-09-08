1f4adedd87e87eccf857ba09ede4cecc17a9bf5c
Vedaprakash88 <vedachintha@gmail.com>
2024-09-08 18:11:26 +0200

    tried using hashmaps to speedup loading images

--- audio_visuals.py
+++ audio_visuals.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from tqdm import tqdm
+from PIL import Image
+from io import BytesIO
+
 
 root = "D:\\10. SRH_Academia\\1. All_Notes\\2. Semester 2\\3. Artificial Intelligence\\Project\\DATA\\genres_original"
 targetdir = "D:\\10. SRH_Academia\\1. All_Notes\\2. Semester 2\\3. Artificial Intelligence\\Project\\DATA\\Temp\\"
@@ -11,6 +14,7 @@ target_folders = os.listdir(root)
 root_folders = os.listdir(targetdir)
 target_folders.sort(reverse=False)
 root_folders.sort(reverse=False)
+img_dict = {}
 
 if target_folders != root_folders:
     folders_to_create = [x for x in target_folders if x not in root_folders]
@@ -18,51 +22,80 @@ if target_folders != root_folders:
         path = os.path.join(targetdir, fldr)
         os.mkdir(path)
 total_files = sum([len(files) for r, d, files in os.walk(root)])
+batch = 100
+loop = 1
+
+def fig_to_image(fig):
+    buf = BytesIO()
+    fig.savefig(buf, format='JPEG', bbox_inches='tight', pad_inches=-0.1)
+    buf.seek(0)
+    img = Image.open(buf)
+    # img = img.convert('RGB') 
+    return img
+
 
 with tqdm(total=total_files, desc="Processing audio files") as pbar:
-    for path, subdirs, files in os.walk(root):
-        for name in files:
-            filename = os.path.join(path, name)
-            y, sr = librosa.load(filename)
+    for pathi, subdirs, files in os.walk(root):
+        for namei in files:
+            filename = os.path.join(pathi, namei)
+            yi, sri = librosa.load(filename)
+            img_dict.update({namei: {"y": yi,
+                                    "sr": sri,
+                                    "path": pathi}
+                            })
+
+    if len(img_dict) > 0:
+        for key in img_dict.keys():
+            y = img_dict.get(key, {}).get("y")
+            sr = img_dict.get(key, {}).get("sr")
+            path = img_dict.get(key, {}).get("path")
+            name = key
+
+
+
             target_folder = os.path.join(targetdir, os.path.basename(os.path.normpath(path)))
-            fig_name = os.path.join(target_folder, name[:len(name) - 4])
+            fig_name = (os.path.join(target_folder, name[:len(name) - 4]))
+
 
             # waveform
-            plt.figure()
-            plt.subplot(1, 1, 1)
-            librosa.display.waveshow(y, sr=sr, axis='time', color='cyan')
-            plt.axis('off')
-            plt.savefig(fig_name + "_Wav.JPEG", bbox_inches='tight', pad_inches=-0.1)
-            plt.close()
+
+            fig, ax = plt.subplots()
+            librosa.display.waveshow(y, sr=sr, ax=ax)
+            ax.set_axis_off()
+            img = fig_to_image(fig)
+            img.save(fig_name + "_Wav.JPEG", "JPEG")
+            plt.close(fig)
 
             # Visualizing MFCC
 
             mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-            fig, ax = plt.subplots(nrows=1, sharex=True)
-            img = librosa.display.specshow(mfccs, x_axis='time')
-            plt.axis('off')
-            plt.savefig(fig_name + "_MFCC.JPEG", bbox_inches='tight', pad_inches=-0.1)
-            plt.close()
+            fig, ax = plt.subplots()
+            librosa.display.specshow(mfccs, x_axis='time', ax=ax)
+            ax.set_axis_off()
+            img = fig_to_image(fig)
+            img.save(fig_name + "_MFCC.JPEG", "JPEG")
+            plt.close(fig)
 
-            # Visualizing Mel_Spec
-
-            S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax = 8000)
-            fig, ax = plt.subplots(nrows=1, sharex = True)
-            img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max),
-                                        x_axis='time', y_axis='mel', fmax=8000)
-            plt.axis('off')
-            plt.savefig(fig_name + "_Mel_Spec.JPEG", bbox_inches='tight', pad_inches=-0.1)
-            plt.close()
-
+            # Mel Spectrogram
+            S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+            fig, ax = plt.subplots()
+            librosa.display.specshow(librosa.power_to_db(S, ref=np.max),
+                                        x_axis='time', y_axis='mel', fmax=8000, ax=ax)
+            ax.set_axis_off()
+            img = fig_to_image(fig)
+            img.save(fig_name + "_Mel_Spec.JPEG", "JPEG")
+            plt.close(fig)
 
             # Spectrogram
-
-            fig, ax = plt.subplots()
             D_highres = librosa.stft(y, hop_length=256, n_fft=4096)
             S_db_hr = librosa.amplitude_to_db(np.abs(D_highres), ref=np.max)
-            img = librosa.display.specshow(S_db_hr, hop_length=256, x_axis='time', y_axis='log')
-            plt.axis('off')
-            plt.savefig(fig_name + "_Spec.JPEG", bbox_inches='tight', pad_inches=-0.1)
-            plt.close()
+            fig, ax = plt.subplots()
+            librosa.display.specshow(S_db_hr, hop_length=256, x_axis='time', y_axis='log', ax=ax)
+            ax.set_axis_off()
+            img = fig_to_image(fig)
+            img.save(fig_name + "_Spec.JPEG", "JPEG")
+            plt.close(fig)
 
             pbar.update(1)
+
+
